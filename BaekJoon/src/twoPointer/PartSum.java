40d8c6c2428287b4ddca08536f61c072a1b8a41e
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-09-10 17:46:25 +0900

    다시 커밋

---
+++ BaekJoon/src/twoPointer/PartSum.java
@@ -0,0 +1,44 @@
+package twoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PartSum {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = stoi(st.nextToken());
+		int S = stoi(st.nextToken());
+		int min = Integer.MAX_VALUE;
+
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = stoi(st.nextToken());
+		}
+		
+		int lt = 0, sum = 0, len = 0;
+		for (int rt = 0; rt < N; rt++) {
+			sum += arr[rt];
+			len++;
+			if(len > 1) {
+				while(sum >= S) {
+					min = Math.min(min, len);
+					sum -= arr[lt++];
+					len--;
+				}
+			}
+		}
+		
+		if(min == Integer.MAX_VALUE) min = 0;
+		System.out.println(min);
+	}
+	
+	public static int stoi(String str) {
+		return Integer.parseInt(str);
+	}
+}
