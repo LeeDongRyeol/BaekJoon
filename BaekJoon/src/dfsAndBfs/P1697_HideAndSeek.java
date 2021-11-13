efb592c4a562810762872775eec307682167bf46
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-11-13 22:55:33 +0900

    백준 1697번 숨바꼭질

---
+++ BaekJoon/src/dfsAndBfs/P1697_HideAndSeek.java
@@ -0,0 +1,87 @@
+package dfsAndBfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author dr854
+ *  https://www.acmicpc.net/problem/1697
+ *  백준 1697번 숨바꼭질
+ *  
+ *  if(cur.loc < sister && moveVal < cur.loc) continue;
+	if(cur.loc > sister && moveVal > cur.loc) continue;
+	
+	위의 로직을 제거하니 제대로 동작하였다.
+ */
+
+public class P1697_HideAndSeek {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Pos subin = new Pos(sc.nextInt());
+		int sister = sc.nextInt();
+		boolean[] visit = new boolean[100001];
+		Queue<Pos> q = new LinkedList<Pos>();
+		q.add(subin);
+		visit[subin.loc] = true;
+		
+		int answer = 0;
+		
+		while(!q.isEmpty()) {
+			Pos cur = q.poll();
+			
+			if(cur.loc == sister) {
+				answer = cur.cnt;
+				break;
+			}
+			
+			for (int i = 1; i <= 3; i++) {
+				int moveVal = move(cur.loc, i);
+				if(cur.loc < sister && moveVal < cur.loc) continue;
+				if(cur.loc > sister && moveVal > cur.loc) continue;
+				
+				if(moveVal > 100000 || moveVal < 0) continue;
+				
+				if(visit[moveVal]) continue;
+				
+				Pos temp = new Pos(moveVal, cur.cnt + 1);
+				
+				visit[temp.loc] = true;
+				
+				q.add(temp);
+			}
+		}
+		
+		System.out.println(answer);
+		sc.close();
+	}
+	
+	public static int move(int pos, int idx) {
+		int loc = pos;
+		
+		switch(idx) {
+			case 1 : loc+=1; break;
+			case 2 : loc-=1; break;
+			case 3 : loc*=2; break;
+		}
+		
+		return loc;
+	}
+	
+	static class Pos {
+		int loc, cnt;
+		
+		public Pos(int loc) {
+			this.loc = loc;
+			this.cnt = 0;
+		}
+		
+		public Pos(int loc, int cnt) {
+			this.loc = loc;
+			this.cnt = cnt;
+		}
+	}
+}
+
+
